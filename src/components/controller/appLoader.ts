c660b02f8762ff2e489802736fecfc33780d759e
Pavel Viarbitski <verbipo@gmail.com>
2023-01-25 15:43:11 +0100

    faet: migration file to ts, clean up

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'be6d70813aee44438323a52e106edc19', // получите свой ключ https://newsapi.org/
+            apiKey: 'be6d70813aee44438323a52e106edc19',
         });
     }
 }
